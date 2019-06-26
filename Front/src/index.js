c132841a4fb8924c5ec98ef07c646e8bbfc1d51a
Daniel Ramos <ramos.danielferreira@gmail.com>
2019-06-25 22:04:07 -0300

    Estrutura minima para criar um projeto

--- Front/src/index.js
+++ Front/src/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { MainPage } from "./components/MainPage";
-import { Survey } from "./components/Survey";
-import { ThankYou } from "./components/ThankYou";
 
 const rootElement = document.getElementById("root");
 
 const App = () => {
     return <Router>
         <Switch>
-            <Route exact path="/" component={MainPage} />
-            <Route exact path="/pesquisa" component={Survey} />
-            <Route exact path="/obrigado" component={ThankYou}></Route>
+            <Route exact path="/" component={() => <h1>It's live</h1>} />
         </Switch>
     </Router>
 }
